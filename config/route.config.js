cb7cb3b19b80ea3a940cb31beaf3145208330b19
Feroz Ahmmed <froghramar@gmail.com>
2017-02-22 12:12:57 +0600

    route config 404

--- config/route.config.js
+++ config/route.config.js
@@ -1,19 +1,29 @@
 (function () {
     var app = angular.module("cric-app");
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("/home");
+    app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+        $urlRouterProvider.otherwise("/404");
         $stateProvider
-          .state('home', {
-              url: "/home",
-              templateUrl: "views/home.html"
-          })
-          .state('match', {
-              url: "/match/:matchid",
-              templateUrl: "views/match.html"
-          })
-          .state('play', {
-              url: "/play",
-              templateUrl: "views/play.html"
-          });
+            .state('base', {
+                url: "",
+                templateUrl: "views/home.html"
+            })
+            .state('home', {
+                url: "/home",
+                templateUrl: "views/home.html"
+            })
+            .state('match', {
+                url: "/match/:matchid",
+                templateUrl: "views/match.html"
+            })
+            .state('play', {
+                url: "/play",
+                templateUrl: "views/play.html",
+                controller: 'play-ctrl'
+            })
+            .state('404', {
+                url: "/404",
+                templateUrl: "views/404.html"
+            });
+        $locationProvider.hashPrefix('');
     });
 })();
